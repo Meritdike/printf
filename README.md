cb0afce28ad1514958d731c8b93e4f6481e22943
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-16 23:24:13 +0000

    Add README.md and comment to main.h

---
+++ README.md
@@ -0,0 +1,2 @@
+## PROJECT TITLE: 0x11. C - printf
+
---
+++ _printf.c
@@ -0,0 +1,10 @@
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: a pointer to a char
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
+
