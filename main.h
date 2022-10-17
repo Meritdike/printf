dfaaf53fd00af9eb9dca5a03a00f83e118990fe3
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-17 18:23:34 +0000

    Update main.h

--- main.h
+++ main.h
@@ -7,7 +7,11 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int format_string(va_list Args, const char *Str);
 int length_of_string(char *string);
+int percent_char_check(int *flag, char character);
 int print_character(va_list arg);
+int print_string(va_list arg);
+int switching(char c, va_list arg);
 
 #endif /* MAIN_H */
