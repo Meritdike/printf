6c2dda4f4acb46cc4334ae5ef559faf39eab1ba9
Merit Dike <dike.merit@yahoo.com>
2022-10-17 10:50:57 -0700

    Update main.h

--- main.h
+++ main.h
@@ -9,5 +9,9 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int length_of_string(char *string);
 int print_character(va_list arg);
+int format_string(va_list Args, const char *Str);
+int percent_char_check(int *flag, char character);
+int print_string(va_list arg);
+int switching(char c, va_list arg);
 
 #endif /* MAIN_H */
