e1567c9e582d57c471a356de8bc65e83c0be88d7
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-17 17:33:57 +0000

    Upated _printf.c

--- _printf.c
+++ _printf.c
@@ -10,26 +10,43 @@
 
 int _printf(const char *format, ...)
 {
-	int i, j;
-	int count = 0;
-	char character;
-	char *string;
-	va_list args;
+	va_list Args;
+	const char *Str;
+	int counter = 0;
+
+	if (format)
+	{
+		va_start(Args, format);
+		Str = format;
+		counter = format_string(Args, Str);
+		va_end(Args);
+		return (counter);
+	}
+	return (-1);
+}
+
+/*
+	char __attribute__((unused)) percent;
+	size_t __attribute__((unused)) i, j;
+	size_t count = 0;
+	char __attribute__((unused)) character;
+	char __attribute__((unused)) *string;
+	va_list __attribute__((unused)) args;
+
 
 	va_start(args, format);
-
 	for (i = 0; *(format + i) != '\0'; ++i)
 	{
 		if (*(format + i) == '%' && *(format + i + 1) == 'c')
 		{
-			i++;
-			character = va_arg(args, int);
+			character = (char)va_arg(args, size_t);
 			write(1, &character, 1);
+			i++;
 		}
 		else if (*(format + i) == '%' && *(format + i + 1) == 's')
 		{
 			i++;
-			string = va_arg(args, char *);
+			string = (char *)va_arg(args, char *);
 			for (j = 0; *(string + j) != '\0'; ++j)
 			{
 				write(1, (string + j), 1);
@@ -37,10 +54,11 @@ int _printf(const char *format, ...)
 			}
 			count--;
 		}
-		else if (*(format + i) == '\\' && *(format + i + 1) == 'n')
+		else if (*(format + i) == '%' && *(format + i + 1) == '%')
 		{
 			i++;
-			write(1, "\n", 1);
+			percent = '%';
+			write(1, &percent, 1);
 		}
 		else
 		{
@@ -51,3 +69,19 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+*/
+
+/*
+size_t check(const char *format, va_list args)
+{
+	va_start(args, format);
+	size_t __attribute__((unused)) format_cnt = 0;
+	size_t __attribute__((unused)) arg_cnt = 0;
+
+	if (*(format + i) == '%' && *(format + i + 1) == 's')
+
+	va_end(args);
+	return (0);
+}
+*/
+
