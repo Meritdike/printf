93a90637f33b9d02e3e5909ae896a0f7c90bd0c2
Vanbliser <ayogublossomisrael@gmail.com>
2022-10-18 18:31:56 +0000

    Update main.h

--- main.h
+++ main.h
@@ -15,6 +15,7 @@ int print_character(va_list arg);
 int format_string(va_list Args, const char *Str);
 int percent_char_check(int *flag, char character);
 int print_string(va_list arg);
+int print_string2(va_list arg);
 int switching1(char c, va_list arg);
 int switching2(char c, va_list arg);
 int switching3(char c, va_list arg);
@@ -22,7 +23,8 @@ int print_signed_int(va_list arg, int base);
 char *base_converter(char hexa_dec[], unsigned int number, int base);
 int print_unsigned_int(va_list arg, int base);
 int print_hexadecimal(va_list arg, char *rep);
-
+int refactor(size_t k, size_t j, size_t length, int counter, int flag,
+char ch, char str[5], char a[16]);
 
 
 #endif /* MAIN_H */
